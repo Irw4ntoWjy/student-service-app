3f5a8b4d9f18a56624a38d497b6bb0c04cecd8e6
Irw4ntoWjy <irwantowijaya0506@gmail.com>
2025-02-06 16:14:10 +0700

    feat: add login page design

--- src/routes/(app)/login/+page.svelte
+++ src/routes/(app)/login/+page.svelte
@@ -1,28 +1,67 @@
 <script lang="ts">
 	import src from '$lib/assets/UPH-Blue.svg';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
-	import Label from '$lib/components/ui/label/label.svelte';
+	import { Eye, EyeOff, KeyRound, User } from 'lucide-svelte';
+
+	let showPassword: boolean = $state(false);
 </script>
 
 <div class="background flex h-screen w-full flex-col items-center justify-center">
-	<div class="h-[36rem] w-[24rem] rounded-md bg-white p-[30px]">
-		<div class="flex h-full w-full flex-col items-center">
+	<div class="h-[42rem] w-[24rem] rounded-md bg-white p-[30px]">
+		<div class="flex h-full w-full flex-col items-center gap-4">
 			<div class="p-4">
 				<img {src} alt="uph-blue" class="h-[5rem] w-[16.5rem]" />
 			</div>
-			<div class="flex flex-col items-center space-y-2">
-				<span class="text-center text-xl font-bold"
+			<div class="flex flex-col items-center">
+				<span class="p-2 text-center text-xl font-bold"
 					>Welcome to Student Service Admin UPH Medan !</span
 				>
 				<span class="text-md text-center font-normal"
 					>If you dont have account before, please click on 'Sign Up' button below to make account</span
 				>
 			</div>
-			<Label />
-			<Input />
 
-			<Label />
-			<Input />
+			<form class="relative w-full rounded-md border bg-white p-1 shadow-md">
+				<label class="flex h-10 w-full items-center">
+					<Input
+						required
+						placeholder="Username"
+						type="text"
+						class="w-full border-none bg-transparent py-2 pl-10 pr-8 text-gray-700 outline-none"
+					/>
+					<div class="absolute left-3 text-gray-500 transition-transform duration-300 ease-in-out">
+						<User class="size-4" />
+					</div>
+				</label>
+			</form>
+
+			<form class="relative w-full rounded-md border bg-white p-1 shadow-md">
+				<label class="relative flex h-10 w-full items-center">
+					<Input
+						required
+						placeholder="Password"
+						type={showPassword ? 'text' : 'password'}
+						class="w-full border-none bg-transparent py-2 pl-10 pr-8 text-gray-700 outline-none"
+					/>
+					<div class="absolute left-3 text-gray-500">
+						<KeyRound class="size-4" />
+					</div>
+
+					<Button
+						type="button"
+						variant="ghost"
+						class="absolute right-3 text-gray-500 focus:outline-none"
+						onclick={() => (showPassword = !showPassword)}
+					>
+						{#if showPassword}
+							<EyeOff class="size-4" />
+						{:else}
+							<Eye class="size-4" />
+						{/if}
+					</Button>
+				</label>
+			</form>
 		</div>
 	</div>
 </div>

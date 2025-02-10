dcd236412394fd2d57e340cfe066bbab21193fee
Irw4ntoWjy <irwantowijaya0506@gmail.com>
2025-02-11 00:26:02 +0700

    feat: commit tertinggal

---
+++ src/routes/(app)/login/input-otp-dialog.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import * as InputOTP from '$lib/components/ui/input-otp/index.js';
+
+	let { open = $bindable(false) }: { open: boolean } = $props();
+</script>
+
+<Dialog.Root bind:open>
+	<Dialog.Content class="">
+		<Dialog.Header>
+			<Dialog.Title></Dialog.Title>
+			<Dialog.Description></Dialog.Description>
+		</Dialog.Header>
+		<InputOTP.Root maxlength={6}>
+			{#snippet children({ cells })}
+				<InputOTP.Group>
+					{#each cells.slice(0, 3) as cell}
+						<InputOTP.Slot {cell} />
+					{/each}
+				</InputOTP.Group>
+				<InputOTP.Separator />
+				<InputOTP.Group>
+					{#each cells.slice(3, 6) as cell}
+						<InputOTP.Slot {cell} />
+					{/each}
+				</InputOTP.Group>
+			{/snippet}
+		</InputOTP.Root>
+	</Dialog.Content>
+</Dialog.Root>
